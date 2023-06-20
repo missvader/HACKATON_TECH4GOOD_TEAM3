c89ca9d5dc6ad648124d1d9fd71e855eacbc8495
Jose <joseramos@MacBook-Air-de-Jose.local>
2023-06-20 17:58:27 +0200

    a

--- backend/src/main/java/cat/tech4good/team3/model/repository/MercatsRepo.java
+++ backend/src/main/java/cat/tech4good/team3/model/repository/MercatsRepo.java
@@ -1,9 +1,13 @@
 package cat.tech4good.team3.model.repository;
 
+import cat.tech4good.team3.model.domain.Fira;
 import cat.tech4good.team3.model.domain.Mercat;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface MercatsRepo extends MongoRepository<Mercat,String>{
+    List<Fira> findByDistricte(String string);
 
 
 
