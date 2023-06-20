352e154be2040b9608410645a72343b19d0b0662
marshalljordan1 <jordan_marshall@live.co.uk>
2023-06-20 15:27:53 +0200

    add link to search page

--- frontend/src/pages/Welcome/Welcome.jsx
+++ frontend/src/pages/Welcome/Welcome.jsx
@@ -1,16 +1,23 @@
 import "../Welcome/welcome.css";
 import Parallax from "../../components/Parallax/Parallax";
+import { Link } from "react-router-dom";
 
 const Welcome = () => {
   return (
     <>
       <Parallax />
-      <div className="welcome-info">
-        <h1 className="welcome-title">La sostenibilitat comença a prop teu</h1>
-        <div className="button-container">
-          <button className="btn btn-primary welcome-button shadow-xl">
-            cerca
-          </button>
+      <div className="welcome-container">
+        <div className="welcome-info">
+          <h1 className="welcome-title">
+            La sostenibilitat comença a prop teu
+          </h1>
+          <div className="button-container">
+            <Link to="/SearchPage">
+              <button className="btn btn-lg btn-primary welcome-button shadow-xl">
+                cerca
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </>
