d6c04b90fb11d33cbcc1a78b73b095072d9bb4c0
missvader <vanepalazon611@gmail.com>
2023-06-20 20:19:25 +0200

    restart jsonserver

--- frontend/src/components/Badges.jsx
+++ frontend/src/components/Badges.jsx
@@ -1,13 +1,21 @@
 const Badges = () => {
   return (
-    <div className="flex space-x-2 my-8">
-      <div className="badge">default</div>
-      <div className="badge badge-neutral">neutral</div>
-      <div className="badge badge-primary">primary</div>
-      <div className="badge badge-secondary">secondary</div>
-      <div className="badge badge-accent">accent</div>
-      <div className="badge badge-ghost">ghost</div>
-      <div className="badge badge-outline">outline</div>
+    <div className="flex space-x-2 my-8 flex-wrap w-4/5 ">
+      <div className="badge">RESTAURANTS</div>
+      <div className="badge badge-neutral">BARS</div>
+      <div className="badge badge-primary">ROBA</div>
+      <div className="badge badge-secondary">PERRUQUERIES</div>
+      <div className="badge badge-accent">SUPERMERCAT</div>
+      <div className="badge badge-ghost">PÀ</div>
+      <div className="badge badge-outline">ESTÈTICA</div>
+      <div className="badge">REPARACIONS</div>
+      <div className="badge badge-neutral">FARMÀCIA I SANITAT</div>
+      <div className="badge badge-primary">CARNISSERIA</div>
+      <div className="badge badge-secondary">FRUITES I VERDURES</div>
+      <div className="badge badge-accent">PEIX</div>
+      <div className="badge badge-ghost">JOIERIA</div>
+      <div className="badge badge-outline">EQUIPAMENT LLAR</div>
+      <div className="badge ">CALÇAT</div>
     </div>
   );
 };
