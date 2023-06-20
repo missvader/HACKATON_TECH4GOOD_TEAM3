42737efb6518d8f83843b63b35e5310edcbad0ac
Nuria Viana <nvianaperez@gmail.com>
2023-06-20 13:08:56 +0200

    structure PriceEvolution PriceEvolutionRepo and ShopCenterService

---
+++ backend/src/main/java/cat/tech4good/team3/model/repository/PriceEvolutionRepo.java
@@ -0,0 +1,9 @@
+package cat.tech4good.team3.model.repository;
+
+import cat.tech4good.team3.model.domain.PriceEvolution;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PriceEvolutionRepo extends MongoRepository<PriceEvolution, Integer> {
+
+
+}
