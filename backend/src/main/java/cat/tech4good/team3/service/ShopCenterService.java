a597b1f772d9efc190d7006f19ae9cc693bd6332
Jose <joseramos@MacBook-Air-de-Jose.local>
2023-06-20 14:00:22 +0200

    controller

--- backend/src/main/java/cat/tech4good/team3/service/ShopCenterService.java
+++ backend/src/main/java/cat/tech4good/team3/service/ShopCenterService.java
@@ -1,9 +1,13 @@
 package cat.tech4good.team3.service;
 
+import cat.tech4good.team3.model.domain.Mercat;
+import cat.tech4good.team3.model.dto.GenericResponseDto;
 import cat.tech4good.team3.model.repository.*;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -19,7 +23,11 @@ public class ShopCenterService {
     @Autowired
     MercatsRepo mercatsRepo;
 
+    public List<Mercat> getMercats(){
+return mercatsRepo.findAll();
 
+
+    }
 
 
 }
